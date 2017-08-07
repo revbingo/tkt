4a5921a0baac8550a49368c8cd77728cc9956aab
Mark Piper <revbingo@gmail.com>
2017-08-07 15:57:57 +0300

    MP: Curse you gitignore!

---
+++ src/main/kotlin/com/revbingo/db/Database.kt
@@ -0,0 +1,66 @@
+package com.revbingo.db
+
+import com.revbingo.aws.Repository
+import com.revbingo.aws.RepositoryViewModel
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.joda.time.DateTime
+import java.math.BigDecimal
+import java.time.ZoneOffset
+
+sealed class DatabaseAccessor() {
+
+    constructor(database: Database): this() {
+        transaction {
+            SchemaUtils.createMissingTablesAndColumns (RepositoryHistory)
+        }
+    }
+
+    open fun persist(repo: Repository) {
+        val viewWrapper = RepositoryViewModel(repo)
+        transaction {
+            RepositoryHistory.insert {
+                it[updateTime] = DateTime(repo.updateTime.toInstant(ZoneOffset.UTC).toEpochMilli())
+                it[loadBalancerCount] = viewWrapper.loadBalancers().count()
+                it[reservedInstanceCount] = viewWrapper.reservedCount() + viewWrapper.unmatchedCount()
+                it[matchedReservationCount] = viewWrapper.reservedCount()
+                it[instanceCount] = viewWrapper.instanceCount()
+                it[runningCount] = viewWrapper.runningCount()
+                it[vpcCount] = viewWrapper.vpcCount()
+                it[databaseCount] = viewWrapper.databases().count()
+                it[domainNameCount] = viewWrapper.domainNames().count()
+                it[volumeCount] = viewWrapper.volumes().count()
+                it[cost] = BigDecimal.valueOf(viewWrapper.totalCostPerHour())
+            }
+        }
+    }
+
+    open fun getHistory(): List<ResultRow> {
+        return transaction {
+            RepositoryHistory.selectAll().toList()
+        }
+    }
+
+    open class H2(dbLocation: String): DatabaseAccessor(Database.connect("jdbc:h2:${dbLocation}", driver = "org.h2.Driver"))
+
+    class H2InMemory(): H2("mem")
+
+    class NoOp: DatabaseAccessor() {
+        override fun persist(repo: Repository) {}
+        override fun getHistory(): List<ResultRow> { return emptyList() }
+    }
+}
+
+object RepositoryHistory: Table() {
+    val updateTime = datetime("updateTime")
+    val reservedInstanceCount = integer("riCount")
+    val matchedReservationCount = integer("matchedRI")
+    val instanceCount = integer("instanceCount")
+    val runningCount = integer("runningCount").default(0)
+    val vpcCount = integer("vpcCount").default(0)
+    val databaseCount = integer("dbCount")
+    val domainNameCount = integer("dnsCount")
+    val loadBalancerCount = integer("elbCount")
+    val volumeCount = integer("ebsCount").default(0)
+    val cost = decimal("totalCost", precision = 10, scale = 2)
+}
